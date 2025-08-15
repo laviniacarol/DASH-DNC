f3418794384f01d8be4b02b815ec386f32cfccdd
laviniacarol <carolxlavinia@gmail.com>
2025-08-14 21:31:29 -0300

    adding hocks in log page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,53 @@
-import { Box, Container } from '@mui/material'
-import { Grid } from '@mui/material'
-import { BannerImage, FormComponent, Logo, StyledH1, StyledP} from '../components'
+import { Box, Container, Grid } from '@mui/material'
+import { BannerImage, FormComponent, Logo, StyledH1, StyledP } from '../components'
 import { pxToRem } from '@/utils'
+import React, { ChangeEvent, useEffect } from 'react'
 
+// hooks
+import { useFormValidation, usePost } from '@/hooks'
+
+// types
+import { MessageProps, LoginData, LoginPostData } from '@/types'
 
 function Login() {
+  const inputs = [
+    { type: 'email', placeholder: 'Email' },
+    { type: 'password', placeholder: 'Senha' }
+  ]
+
+  const { data, loading, error, postData } = usePost<LoginData, LoginPostData>('login')
+  const { formValues, formValid, handleChange } = useFormValidation(inputs)
+
+  const handleMessage = (): MessageProps => {
+    if (!error) return { msg: '', type: 'success' }
+    switch (error) {
+      case 401:
+        return {
+          msg: 'Email e/ou senha inválidos',
+          type: 'error',
+        }
+      default:
+        return {
+          msg: 'Não foi possível realizar a operação. Entre em contato com o suporte.',
+          type: 'error',
+        }
+    }
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    await postData({
+      email: String(formValues[0]),
+      password: String(formValues[1])
+    })
+  }
+
+  useEffect(() => {
+    if (data?.jwt_token) {
+      console.log('DATA: ', data)
+    }
+  }, [data])
+
   return (
     <Box>
       <Grid container>
@@ -19,26 +62,33 @@ function Login() {
           }}
         >
           <Container maxWidth="sm">
-            <Box sx={{ marginBottom: pxToRem(24)}}><Logo height={41} width={100} /></Box>
-            <Box sx={{ marginBottom: pxToRem(24)}}>
-              <StyledH1 color="inherit">Bem-vindo</StyledH1>
-              <StyledP color="inherit">Digite sua senha e email para logar</StyledP>
+            <Box sx={{ marginBottom: pxToRem(24) }}>
+              <Logo height={41} width={100} />
             </Box>
 
-            <FormComponent 
-            input={[
-              { type: 'email', placeholder: 'Email'},
-              { type: 'password', placeholder: 'Senha'},
-            ]} 
+            <Box sx={{ marginBottom: pxToRem(24) }}>
+              <StyledH1 color="inherit">Bem-vindo</StyledH1>
+              <StyledP color="inherit">Digite seu email e senha para logar</StyledP>
+            </Box>
 
-            button={[
-              { className: 'primary', type: 'submit', children: 'Login'},
-            ]}
-
-            message={{
-               msg: "Sucesso",
-               type: "success"
-            }}
+            <FormComponent
+              input={inputs.map((input, index) => ({
+                type: input.type,
+                placeholder: input.placeholder,
+                value: formValues[index] || '',
+                onChange: (e: ChangeEvent<HTMLInputElement>) =>
+                  handleChange(index, (e.target as HTMLInputElement).value)
+              }))}
+              button={[
+                {
+                  className: 'primary',
+                  disabled: !formValid || loading,
+                  type: 'submit',
+                  onClick: handleSubmit,
+                  children: loading ? 'Aguarde' : 'Login',
+                },
+              ]}
+              message={handleMessage()}
             />
           </Container>
         </Grid>
