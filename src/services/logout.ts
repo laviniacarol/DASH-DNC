4aa04de4383e04497d2f97ba853674d6c50e976b
laviniacarol <carolxlavinia@gmail.com>
2025-08-17 00:59:56 -0300

    Feat Logout Flow

---
+++ src/services/logout.ts
@@ -0,0 +1,9 @@
+import Cookies from "js-cookie";
+
+export function logout() {
+    if(confirm('Deseja seguir com o logout?') === true) {
+      
+        Cookies.remove('Authorization')
+        window.location.href ='/'
+    }
+}
